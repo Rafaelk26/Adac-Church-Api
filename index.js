213a776ff474e8a653582ac228b16f39142a48d0
Rafaelk26 <rafaelk262002@gmail.com>
2024-06-05 19:29:34 -0300

    update for once do request in back-end

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const NodeCache = require('node-cache');
 
 const app = express();
-const cache = new NodeCache({ stdTTL: 259200 });
+const cache = new NodeCache({ stdTTL: 216000 });
 const PORT = 3001;
 
 const API_KEY = 'AIzaSyCGLK8e2xkpGIZC_c31GQSpsLq0qYstrFM';
@@ -14,57 +14,13 @@ app.use(cors());
 
 app.get('/api/videos', async (req, res) => {
   const cachedVideos = cache.get('videos');
-  const lastVideoId = cache.get('lastVideoId');
 
-  if (cachedVideos && lastVideoId) {
-    try {
-      // Verifica se há novos vídeos
-      const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-        params: {
-          key: API_KEY,
-          channelId: CHANNEL_ID,
-          part: 'snippet',
-          order: 'date',
-          maxResults: 1
-        }
-      });
-
-      const latestVideoId = response.data.items[0].id.videoId;
-
-      if (lastVideoId === latestVideoId) {
-        // Se o vídeo mais recente não mudou, usa o cache
-        return res.json(cachedVideos);
-      }
-
-      // Se mudou, atualiza o cache com novos dados
-      const videoResponse = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-        params: {
-          key: API_KEY,
-          channelId: CHANNEL_ID,
-          part: 'snippet',
-          order: 'date',
-          maxResults: 5
-        }
-      });
-
-      const videos = videoResponse.data.items.map(item => ({
-        title: item.snippet.title,
-        description: item.snippet.description,
-        thumbnail: item.snippet.thumbnails.default.url,
-        videoId: item.id.videoId
-      }));
-
-      cache.set('videos', videos);
-      cache.set('lastVideoId', latestVideoId);
-
-      return res.json(videos);
-    } catch (error) {
-      console.error('Erro ao verificar novos vídeos:', error.response ? error.response.data : error.message);
-      return res.status(500).json({ error: 'Erro ao verificar novos vídeos' });
-    }
+  if (cachedVideos) {
+    // Retorna vídeos do cache se disponíveis
+    return res.json(cachedVideos);
   } else {
-    // Cache inicial ou cache expirado
     try {
+      // Faz a requisição à API do YouTube
       const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
         params: {
           key: API_KEY,
@@ -82,10 +38,8 @@ app.get('/api/videos', async (req, res) => {
         videoId: item.id.videoId
       }));
 
-      const latestVideoId = response.data.items[0].id.videoId;
-
+      // Armazena os vídeos no cache
       cache.set('videos', videos);
-      cache.set('lastVideoId', latestVideoId);
 
       return res.json(videos);
     } catch (error) {
