fc9173045a3159c617af6c982c7d12dfc71a12e8
Rafaelk26 <rafaelk262002@gmail.com>
2024-06-06 17:04:59 -0300

    update with archive cache.json add

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const axios = require('axios');
+const fs = require('fs');
 const cors = require('cors');
-const NodeCache = require('node-cache');
 
 const app = express();
-const cache = new NodeCache({ stdTTL: 216000 });
 const PORT = 3001;
 
 const API_KEY = 'AIzaSyCGLK8e2xkpGIZC_c31GQSpsLq0qYstrFM';
 const CHANNEL_ID = 'UCHG4HehgHbGnPZD0bWPBD9g';
+const CACHE_FILE = 'cache.json';
+const CACHE_DURATION = 60 * 60 * 1000 * 60;
 
 app.use(cors());
 
-app.get('/api/videos', async (req, res) => {
-  const cachedVideos = cache.get('videos');
+const saveCache = (data) => {
+  fs.writeFileSync(CACHE_FILE, JSON.stringify({
+    timestamp: Date.now(),
+    data
+  }));
+};
 
-  if (cachedVideos) {
-    // Retorna vídeos do cache se disponíveis
-    return res.json(cachedVideos);
-  } else {
-    try {
-      // Faz a requisição à API do YouTube
+const loadCache = () => {
+  if (fs.existsSync(CACHE_FILE)) {
+    const cache = JSON.parse(fs.readFileSync(CACHE_FILE, 'utf-8'));
+    if (Date.now() - cache.timestamp < CACHE_DURATION) {
+      return cache.data;
+    }
+  }
+  return null;
+};
+
+app.get('/api/videos', async (req, res) => {
+  try {
+    let videos = loadCache();
+
+    if (!videos) {
       const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
         params: {
           key: API_KEY,
@@ -31,21 +45,20 @@ app.get('/api/videos', async (req, res) => {
         }
       });
 
-      const videos = response.data.items.map(item => ({
+      videos = response.data.items.map(item => ({
         title: item.snippet.title,
         description: item.snippet.description,
         thumbnail: item.snippet.thumbnails.default.url,
         videoId: item.id.videoId
       }));
 
-      // Armazena os vídeos no cache
-      cache.set('videos', videos);
-
-      return res.json(videos);
-    } catch (error) {
-      console.error('Erro ao buscar vídeos:', error.response ? error.response.data : error.message);
-      return res.status(500).json({ error: 'Erro ao buscar vídeos' });
+      saveCache(videos);
     }
+
+    res.json(videos);
+  } catch (error) {
+    console.error('Erro ao buscar vídeos:', error.response ? error.response.data : error.message);
+    res.status(500).json({ error: 'Erro ao buscar vídeos' });
   }
 });
 
