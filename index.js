904d1467f530ca14920bfe32ecdcb5b0b0cf62cf
Rafaelk26 <rafaelk262002@gmail.com>
2024-06-06 17:24:47 -0300

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const API_KEY = 'AIzaSyCGLK8e2xkpGIZC_c31GQSpsLq0qYstrFM';
 const CHANNEL_ID = 'UCHG4HehgHbGnPZD0bWPBD9g';
@@ -14,18 +14,28 @@ const CACHE_DURATION = 60 * 60 * 1000 * 60;
 app.use(cors());
 
 const saveCache = (data) => {
-  fs.writeFileSync(CACHE_FILE, JSON.stringify({
-    timestamp: Date.now(),
-    data
-  }));
+  try {
+    fs.writeFileSync(CACHE_FILE, JSON.stringify({
+      timestamp: Date.now(),
+      data
+    }));
+    console.log('Cache salvo com sucesso.');
+  } catch (error) {
+    console.error('Erro ao salvar o cache:', error.message);
+  }
 };
 
 const loadCache = () => {
-  if (fs.existsSync(CACHE_FILE)) {
-    const cache = JSON.parse(fs.readFileSync(CACHE_FILE, 'utf-8'));
-    if (Date.now() - cache.timestamp < CACHE_DURATION) {
-      return cache.data;
+  try {
+    if (fs.existsSync(CACHE_FILE)) {
+      const cache = JSON.parse(fs.readFileSync(CACHE_FILE, 'utf-8'));
+      if (Date.now() - cache.timestamp < CACHE_DURATION) {
+        console.log('Cache carregado com sucesso.');
+        return cache.data;
+      }
     }
+  } catch (error) {
+    console.error('Erro ao carregar o cache:', error.message);
   }
   return null;
 };
